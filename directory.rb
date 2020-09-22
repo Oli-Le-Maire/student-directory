0320b9f1a38ead1cd9a8f531f3f7d95afd2357f3
Oli Le Maire <olilemaire85@gmail.com>
2020-09-22 12:34:08 +0100

    Commit_7 Created methods that take user inputs, with comments taken out from previous version

--- directory.rb
+++ directory.rb
@@ -36,38 +36,3 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
-
-
-
-
-
-# #first, we print the list of students
-# students = [
-#   {name: "Dr. Hannibal Lecter",  cohort: :november},
-#   {name: "Darth Vader",  cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Kruger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Barathon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
- # def print_header
- #   puts "The students of Villains Academy"
- #   puts "-------------"
- # end
-# def print(students)
-#   students.each do |student|
-#     puts "#{student[:name]} (#{student[:cohort]} cohort)"
-#   end
-# end
- # def print_footer(students)
- #   puts "Overall, we have #{students.count} great students"
- # end
-# #nothing happens until we call the methods
-# print_header
-# print(students)
-# print_footer(students)
